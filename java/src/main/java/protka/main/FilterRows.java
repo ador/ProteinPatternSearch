f085e9ab40a5ac6f8d17f1b197375f8776cc9624
Adrienn Szabo <adrienn.szabo4@gmail.com>
2015-02-23 15:39:13 +0100

    Refactoring FilterRows and creating output directory

--- java/src/main/java/protka/main/FilterRows.java
+++ java/src/main/java/protka/main/FilterRows.java
@@ -12,50 +12,48 @@ import protka.filter.SwissprotRowFilter;
 public class FilterRows {
   private static final String[] requiredProps = { "inputDatFile",
       "outputDatFile" };
-
+  private PropertyHandler propHandler = new PropertyHandler(requiredProps);
+  private FileInputStream is;
+  private FileOutputStream os;
+  private Properties properties; 
+  
+  private void createOutDirsAndFile(String outDatPath) throws IOException {
+    String outDatDir = outDatPath.substring(0, outDatPath.lastIndexOf(File.separator));
+    File targetFile = new File(outDatDir); 
+    targetFile.mkdirs();
+    os = new FileOutputStream(outDatPath);
+  }
+  
+  private void filter() throws FileNotFoundException, IOException {
+    if (propHandler.checkProps(properties)) {
+      SwissprotRowFilter swissprotRowFilter = new SwissprotRowFilter();
+      is = new FileInputStream(properties.getProperty("inputDatFile"));
+      createOutDirsAndFile(properties.getProperty("outputDatFile"));
+      if (properties.containsKey("throwOutRows")) {
+        String[] throwOutRows = properties.getProperty(
+            "throwOutRows").split(",");
+        for (String pattern : throwOutRows)
+          swissprotRowFilter.addFilterPattern(pattern);
+      }
+      swissprotRowFilter.setInputDat(is);
+      swissprotRowFilter.setOutputDat(os);
+      swissprotRowFilter.filter();
+      os.flush();
+      os.close();
+    }
+  }
+  
   public static void main(String args[]) throws IOException {
     if (args.length != 1) {
       System.out.println("Usage: FilterRows settings.properties");
       return;
     }
-
-    PropertyHandler propHandler = new PropertyHandler(requiredProps);
-    SwissprotRowFilter swissprotRowFilter = new SwissprotRowFilter();
-
-    FileInputStream is;
-    FileOutputStream os;
-
+    FilterRows fr = new FilterRows();
     try {
-      Properties properties = propHandler.readPropertiesFile(args[0]);
-
-      is = new FileInputStream(
-          properties.getProperty("inputDatFile"));
-      File targetFile = new File(properties.getProperty("outputDatFile")); 
-      targetFile.mkdirs();
-      os = new FileOutputStream(
-          properties.getProperty("outputDatFile"));
-
-      swissprotRowFilter.setInputDat(is);
-      swissprotRowFilter.setOutputDat(os);
-
-      if (propHandler.checkProps(properties)) {
-
-        if (properties.containsKey("throwOutRows")) {
-          String[] throwOutRows = properties.getProperty(
-              "throwOutRows").split(",");
-          for (String pattern : throwOutRows)
-            swissprotRowFilter.addFilterPattern(pattern);
-        }
-
-        swissprotRowFilter.filter();
-        os.flush();
-        os.close();
-
-      }
-
+      fr.properties = fr.propHandler.readPropertiesFile(args[0]);
+      fr.filter();
     } catch (FileNotFoundException e) {
       e.printStackTrace();
-
     } catch (IOException e) {
       e.printStackTrace();
     }
