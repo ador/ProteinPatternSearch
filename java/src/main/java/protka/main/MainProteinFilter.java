0b6ca99c1b124e263f0cd9a3848848dd08e84d2d
Adrienn Szabo <adrienn.szabo4@gmail.com>
2015-03-09 17:07:49 +0100

    refactoring MainProteinFilter.java

--- java/src/main/java/protka/main/MainProteinFilter.java
+++ java/src/main/java/protka/main/MainProteinFilter.java
@@ -4,6 +4,9 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
@@ -18,95 +21,122 @@ import protka.io.SwissprotWriter;
 
 public class MainProteinFilter {
   
+  FileInputStream inDat;
+  FileInputStream inFasta;
+  FileOutputStream outFasta;
+  FileOutputStream outDat;
+  Properties properties;
+  int minSeqLen = 25;
+  SwissprotReader swissprotReader;
+  SwissprotWriter swissprotWriter;
+  FastaReader fastaReader;
+  FastaWriter fastaWriter;
+
   
   private static final String[] requiredProps = { "inputDatFile",
       "outputFastaFile", "inputFastaFile", "outputDatFile" };
 
+  public void createDirsAndFiles(Properties properties) throws IOException {
+    Path pathToFastaFile = Paths.get(properties.getProperty("outputFastaFile"));
+    Files.createDirectories(pathToFastaFile.getParent());
+    Files.createFile(pathToFastaFile);
+
+    Path pathToDatFile = Paths.get(properties.getProperty("outputDatFile"));
+    Files.createDirectories(pathToDatFile.getParent());
+    Files.createFile(pathToDatFile);
+
+    inDat = new FileInputStream(
+        properties.getProperty("inputDatFile"));
+    inFasta = new FileInputStream(
+        properties.getProperty("inputFastaFile"));
+    outFasta = new FileOutputStream(
+        properties.getProperty("outputFastaFile"));
+    outDat = new FileOutputStream(
+        properties.getProperty("outputDatFile"));
+  }
+  
+  public void closeFiles() throws IOException {
+    swissprotWriter.closeOS();
+    inDat.close();
+    inFasta.close();
+    outFasta.close(); 
+  }
+  
+  public void compute() throws IOException {
+    fastaReader = new FastaReader(inFasta);
+    Map<String, FastaItem> fastaMap = new HashMap<String, FastaItem>(600000);
+
+    int counter = 1;
+    FastaItem fastaItem = fastaReader.getNextFastaItem();
+    while (fastaItem != null) {
+      fastaMap.put(fastaItem.acNum, fastaItem);
+      fastaItem = fastaReader.getNextFastaItem();
+      if (counter % 10000 == 0) {
+        System.out.println("Read " + counter + " fasta items.");
+      }
+      ++counter;
+    }
+
+    ProteinFilter proteinFilter = new ProteinFilter();
+
+    // optional property
+    if (properties.containsKey("minSeqLength")) {
+      minSeqLen = Integer.parseInt(properties.getProperty("minSeqLength"));
+      proteinFilter.setMinSeqLen(minSeqLen);
+    }
+
+    if (properties.containsKey("matchProteinPatterns")) {
+      String[] patterns = properties.getProperty(
+          "matchProteinPatterns").split("\\|");
+      for (String pattern : patterns) {
+        String[] kvPair = pattern.split(":");
+        String key = kvPair[0];
+        String value = kvPair[1];
+        proteinFilter.addPattern(key, value);
+      }
+    }
+
+    swissprotReader = new SwissprotReader(inDat);
+    swissprotWriter = new SwissprotWriter(outDat);
+    fastaWriter = new FastaWriter(outFasta);
+
+    Protein protein;
+
+    counter = 1;
+    while ((protein = swissprotReader.getNextProtein()) != null) {
+      if (proteinFilter.match(protein)
+          && fastaMap.containsKey(protein.getAcNum())) {
+        swissprotWriter.writeProtein(protein);
+        fastaWriter.writeFastaItem(fastaMap.get(protein.getAcNum()));
+        
+        if (counter % 1000 == 0) {
+          System.out
+              .println("Wrote " + counter + " fasta items and proteins.");
+        }
+        ++counter;
+      }
+    }
+
+  }
+  
+  
   public static void main(String args[]) {
     if (args.length != 1) {
       System.out.println("Usage: MainProteinFilter settings.properties");
       return;
     }
-
-    int minSeqLen = 25;
-    
+    // create
+    MainProteinFilter main = new MainProteinFilter();
     PropertyHandler propHandler = new PropertyHandler(requiredProps);
 
-    FileInputStream inDat;
-    FileInputStream inFasta;
-    FileOutputStream outFasta;
-    FileOutputStream outDat;
-    Properties properties = propHandler.readPropertiesFile(args[0]);
+    main.properties = propHandler.readPropertiesFile(args[0]);
 
     try {
-      if (propHandler.checkProps(properties)) {
-        inDat = new FileInputStream(
-            properties.getProperty("inputDatFile"));
-        inFasta = new FileInputStream(
-            properties.getProperty("inputFastaFile"));
-        outFasta = new FileOutputStream(
-            properties.getProperty("outputFastaFile"));
-        outDat = new FileOutputStream(
-            properties.getProperty("outputDatFile"));
+      if (propHandler.checkProps(main.properties)) {
 
+        main.createDirsAndFiles(main.properties);        
         
-        FastaReader fastaReader = new FastaReader(inFasta);
-        Map<String, FastaItem> fastaMap = new HashMap<String, FastaItem>(600000);
-
-        int counter = 1;
-        FastaItem fastaItem = fastaReader.getNextFastaItem();
-        while (fastaItem != null) {
-          fastaMap.put(fastaItem.acNum, fastaItem);
-          fastaItem = fastaReader.getNextFastaItem();
-          if (counter % 10000 == 0) {
-            System.out.println("Read " + counter + " fasta items.");
-          }
-          ++counter;
-        }
-
-        ProteinFilter proteinFilter = new ProteinFilter();
-
-        // optional property
-        if (properties.containsKey("minSeqLength")) {
-          minSeqLen = Integer.parseInt(properties.getProperty("minSeqLength"));
-          proteinFilter.setMinSeqLen(minSeqLen);
-        }
-
-        if (properties.containsKey("matchProteinPatterns")) {
-          String[] patterns = properties.getProperty(
-              "matchProteinPatterns").split("\\|");
-          for (String pattern : patterns) {
-            String[] kvPair = pattern.split(":");
-            String key = kvPair[0];
-            String value = kvPair[1];
-            proteinFilter.addPattern(key, value);
-          }
-        }
-
-        SwissprotReader swissprotReader = new SwissprotReader(inDat);
-        SwissprotWriter swissprotWriter = new SwissprotWriter(outDat);
-        FastaWriter fastaWriter = new FastaWriter(outFasta);
-        Protein protein;
-
-        counter = 1;
-        while ((protein = swissprotReader.getNextProtein()) != null) {
-          if (proteinFilter.match(protein)
-              && fastaMap.containsKey(protein.getAcNum())) {
-            swissprotWriter.writeProtein(protein);
-            fastaWriter.writeFastaItem(fastaMap.get(protein.getAcNum()));
-            
-            if (counter % 1000 == 0) {
-              System.out
-                  .println("Wrote " + counter + " fasta items and proteins.");
-            }
-            ++counter;
-          }
-        }
-
-        swissprotWriter.closeOS();
-        inDat.close();
-        inFasta.close();
-        outFasta.close();
+        main.compute();
       }
     } catch (FileNotFoundException e) {
       e.printStackTrace();
